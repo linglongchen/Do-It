f81b04f75d9ee5d0eccea524adfa285526e91188
linglongchen <cll022034@gmail.com>
2020-12-16 10:37:17 +0800

    doit

---
+++ boot-starter-common/src/main/java/com/micro/fast/boot/starter/common/util/LogBean.java
@@ -0,0 +1,20 @@
+package com.micro.fast.boot.starter.common.util;
+
+
+//import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.util.Date;
+
+//@Document(collection = "t_log")
+public class LogBean {
+    private String id;
+    private Integer userId;
+    private String username;
+    @DateTimeFormat(pattern="yyyy-MM-dd")
+    private Date createDate;
+    private String ip;
+    private String className;//类名
+    private String method;//方法名
+    private String reqParam;//请求
+}
---
+++ config-server/src/main/java/com/micro/fast/config/server/ConfigServerApplication.java
@@ -0,0 +1,27 @@
+package com.micro.fast.config.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * 配置中心启动类
+ * @author lishouyu 18332763730@163.com 2017/10/02
+ * @version 1.0
+ * @since 1.0
+ */
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@EnableConfigServer
+@EnableDiscoveryClient
+public class ConfigServerApplication {
+
+	/**
+	 * 配置中心方法　
+	 * @param args 控制台参数
+	 */
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigServerApplication.class, args);
+	}
+}
